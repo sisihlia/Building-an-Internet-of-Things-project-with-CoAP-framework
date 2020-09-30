11cd6f7c09d7c3c6644f19407180548e1b72b732
sisihlia.yuniyarti <sisihlia.yuniyarti@gmail.com>
2020-09-30 21:10:41 +0200

    remove indents from Run.java

--- IoT_Project/src/test/Run.java
+++ IoT_Project/src/test/Run.java
@@ -8,23 +8,18 @@ public class Run {
 
 	public static void main(String[] args) {
 		 try{   // get the collection name in indicated database
-			   mongodb mdb = new mongodb();
-			   test test = new test();
-			   MongoClient mongo = new MongoClient("localhost", 27017);
-			  
+			mongodb mdb = new mongodb();
+			test test = new test();
+			MongoClient mongo = new MongoClient("localhost", 27017);
 			DB db = mongo.getDB("catdemo");
-			 DBCollection collection = db.getCollection("items");
-			 
-		String y = mdb.selectSingleRecordAndFieldByRecordNumber(collection);
-		
-			test.postConnection(y);
-			//test.getValue();
+			DBCollection collection = db.getCollection("items");
+			String y = mdb.selectSingleRecordAndFieldByRecordNumber(collection);
+		        test.postConnection(y);
+			test.getValue();
 			test.observeValue();
 			 
 		 }catch(Exception e){
 		     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 		  }
-
 	}
-
 }
